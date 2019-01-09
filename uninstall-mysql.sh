12b5386bca5bc0653b43b0a8daeb0c68765ac359
JY29 <344159020@qq.com>
2019-01-09 16:02:26 +0800

    clean mysql

---
+++ uninstall-mysql.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "--------uninstalling mysql-----------"
+sudo rm /usr/local/mysql
+sudo rm -rf /usr/local/mysql*
+sudo rm -rf /Library/StartupItems/MySQLCOM
+sudo rm -rf /Library/PreferencePanes/My*
+rm -rf ~/Library/PreferencePanes/My*
+sudo rm -rf /Library/Receipts/mysql*
+sudo rm -rf /Library/Receipts/MySQL*
+sudo rm -rf /var/db/receipts/com.mysql.*
+echo "mysql has been removed from your mac!"
